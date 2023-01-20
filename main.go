0155da25addd81cb8bf71e197f768a408ae30345
senchi <senchpimy@gmail.com>
2023-01-20 13:23:56 -0600

    send css

--- main.go
+++ main.go
@@ -1,41 +1,26 @@
 package main
 
-import ("fmt"
-	"net/http"
+import (
+	"fmt"
 	"io"
+	"net/http"
 	"strings"
-	"regexp"
 )
 
 const domain = "https://senchpimy.xyz"
-//const domain = "https://senchpimy.xyz"
 
 func main() {
 	fmt.Println("Server Start")
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		//fmt.Fprint(w,request("/"))
 		path := r.URL.Path
-		if path!="/"{
-			fmt.Println("hola"+path)
-			html:=request(path)
-			//re := regexp.MustCompile(`src="(.*?)"`)
-			re := regexp.MustCompile(`src=".*."`)
-			matches := re.FindAllStringSubmatch(html, -1)
-			for _, match := range matches {
-				fmt.Println(match)
-			}
-			if strings.HasSuffix(path,".html"){
-			fmt.Fprint(w,html)
-			}
-			if strings.HasSuffix(path,".jpg"){
-				html:=request(path)
-				fmt.Fprint(w,html)
-			}
-		}else {
+		if strings.HasSuffix(path,".css"){
+			w.Header().Set("Content-Type", "text/css")
 			html:=request(path)
 			fmt.Fprint(w,html)
-
 		}
+		html:=request(path)
+		fmt.Fprint(w,html)
 	})
 
         http.ListenAndServe(":3000", nil)
@@ -44,11 +29,13 @@ func main() {
  func request(url string)(foo string){
 	resp, err := http.Get(domain+url)
 	if err != nil {
+		fmt.Println("erro get")
 	    // handle error
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
+		fmt.Println("erro read")
 	    // handle error
 	}
 	return string(body)
